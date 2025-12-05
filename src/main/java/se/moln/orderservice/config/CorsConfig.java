30589e17615af4de2d6d1d0a657a8c4da20aad2b
Student Id <109276629+IsmeteHani@users.noreply.github.com>
2025-12-05 14:35:00 +0100

    Fix CorsConfig for Spring WebFlux reactive stack

--- src/main/java/se/moln/orderservice/config/CorsConfig.java
+++ src/main/java/se/moln/orderservice/config/CorsConfig.java
@@ -3,8 +3,8 @@ package se.moln.orderservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,7 +13,7 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(Arrays.asList(
@@ -33,6 +33,6 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        return new CorsWebFilter(source);
     }
 }
